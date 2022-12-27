6cca1711171e7f841cfa52df8fb01b37af146be1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-12-28 00:33:22 +0600

    Created WorldStatesScreen pages and Updates all pages

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'package:covid_app/view/world_states.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 
 class SplashScreen extends StatefulWidget {
@@ -10,13 +13,19 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
 
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this)..repeat();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _controller.dispose();
 
-
+    Timer(const Duration(seconds: 5),
+        () => Navigator.push(context,MaterialPageRoute(builder: (context) => WorldStatesScreen()))
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -27,6 +36,30 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
           crossAxisAlignment: CrossAxisAlignment.center,
           children:  [
 
+            AnimatedBuilder(
+                animation: _controller,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: const Center(
+                    child: Image(image: AssetImage('images/virus.png')),
+                  ),
+                ),
+                builder: (BuildContext context,Widget? child){
+                  return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                    child: child,
+                  );
+                }),
+            SizedBox(height: MediaQuery.of(context).size.height * .08,),
+            Align(
+              alignment: Alignment.center,
+              child: Text('Covid-19\nTracker App',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                fontWeight: FontWeight.bold,fontSize: 25
+              ),),
+            )
           ],
         ),
       ),
