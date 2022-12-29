0c728c4a3168c07918fd742eb7388c3121e06392
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-12-29 15:38:51 +0600

    Created Countries List Screen page and updates many pages

---
+++ lib/Services/states_services.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../Model/world_states_model.dart';
+import 'Utilities/app_url.dart';
+
+class StatesServices {
+
+
+  Future<WorldStatesModel> fetchWorldStatesRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+}
