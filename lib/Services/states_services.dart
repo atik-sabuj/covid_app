b6be9e77dfdf2f8533e349c57696550c899af99b
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-12-30 17:09:10 +0600

    updates Countries List Screen page and updates many pages

--- lib/Services/states_services.dart
+++ lib/Services/states_services.dart
@@ -16,4 +16,18 @@ class StatesServices {
       throw Exception('Error');
     }
   }
+
+  Future<List<dynamic>> countriesListApi() async {
+    var data ;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+    print(response.statusCode.toString());
+    print(data);
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body.toString());
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
+
 }
